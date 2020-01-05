a06380d73db603cf1e3d33a7c27ff1cacce90a46
Enchan <enchant0130@gmail.com>
2020-01-05 13:46:51 +0900

    Initial Commit

---
+++ CameraEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraEx
+//
+//  Created by EnchantCode on 2020/01/05.
+//  Copyright © 2020 EnchantCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
