04b7faa447dc86dc3357d3116a66fb6c7914acec
Enchan <enchant0130@gmail.com>
2021-01-15 13:58:03 +0900

    [Fix] image orientation

--- CameraEx/ViewController.swift
+++ CameraEx/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  CameraEx
 //
-//  Created by EnchantCode on 2020/01/05.
+//  Created by EnchantCode on 2021/01/14.
 //  Copyright © 2020 EnchantCode. All rights reserved.
 //
 
@@ -10,25 +10,28 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    //--
+    //
     @IBOutlet weak var imageView: UIImageView!
     
-    //--キャプチャセッション
+    // キャプチャセッション
     var captureSesion = AVCaptureSession()
-    //--デバイス
+    // デバイス
     var mainCamera: AVCaptureDevice?
     var innerCamera: AVCaptureDevice?
     var currentDevice: AVCaptureDevice?
-    //--出力
+    // 出力
     var photoOutput: AVCaptureVideoDataOutput?
-    //--表示レイヤ
+    // 表示レイヤ
     var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
     
-    //--
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //--
+        // 向き変更を検知
+        NotificationCenter.default.addObserver(self, selector: #selector(onOrientationChanged(_:)), name: UIDevice.orientationDidChangeNotification, object: nil)
+        
+        // セットアップ
         setupCaptureSession()
         setupDevice()
         setupCameraIO()
@@ -36,31 +39,42 @@ class ViewController: UIViewController {
         captureSesion.startRunning()
     }
     
-    //--タップされたら画像を保存
+    // 向きが変更された時の処理
+    @objc func onOrientationChanged(_ sender: Any){
+        // lava casted
+    }
+    
+    // 撮影ボタンがタップされたとき
     @IBAction func onTapShot(_ sender: Any) {
         let settings = AVCapturePhotoSettings()
         settings.flashMode = .off
-        if var _:AVCaptureConnection = self.photoOutput?.connection(with: AVMediaType.video){
-            let image = self.imageView.image!
-            UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
-        }
+        
+        guard let _ = self.photoOutput?.connection(with: AVMediaType.video) else {return}
+        
+        // 画像データを取得
+        print("Get image data...")
+        guard let image = self.imageView.image else {return}
+        
+        // 保存!
+        print("Saved.")
+        UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
     }
 }
 
-//---カメラ設定用メソッド
+// -カメラ設定用メソッド
 extension ViewController{
     
-    //--カメラの設定
+    // カメラの設定
     func setupCaptureSession(){
         self.captureSesion.sessionPreset = AVCaptureSession.Preset.photo
     }
     
-    //--デバイスの設定
+    // デバイスの設定
     func setupDevice(){
-        //--プロパティを「実装位置指定なし」で設定
+        // プロパティを「実装位置指定なし」で設定
         let devicediscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
         
-        //--プロパティに従ってカメラを探し、前と後ろのカメラをそれぞれinner,maincameraに設定
+        // プロパティに従ってカメラを探し、前と後ろのカメラをそれぞれinner,maincameraに設定
         let devices = devicediscoverySession.devices
         for device in devices{
             if device.position == AVCaptureDevice.Position.back{
@@ -70,23 +84,28 @@ extension ViewController{
             }
         }
         
+        // TODO: アクティブ切り替え機能
         currentDevice = mainCamera
     }
-
-    //--入出力データの設定
+    
+    // 入出力データの設定
     func setupCameraIO(){
         do {
-            //--入力
+            // 入力
             let captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice!)
             captureSesion.addInput(captureDeviceInput)
             
-            //--出力
+            // 出力
             photoOutput = AVCaptureVideoDataOutput()
             photoOutput?.setSampleBufferDelegate(self, queue: DispatchQueue.main)
             
+            // デバイス設定
             do{
                 try currentDevice?.lockForConfiguration()
-                currentDevice?.activeVideoMaxFrameDuration = CMTimeMake(value: 1, timescale: 60)
+                if  let frameRateRange = currentDevice?.activeFormat.videoSupportedFrameRateRanges.first{
+                    currentDevice?.activeVideoMaxFrameDuration = frameRateRange.maxFrameDuration
+                    currentDevice?.activeVideoMaxFrameDuration = frameRateRange.minFrameDuration
+                }
                 currentDevice?.unlockForConfiguration()
             } catch {
                 print(error)
@@ -98,25 +117,25 @@ extension ViewController{
         }
     }
     
-    //--レイヤの設定
+    // レイヤの設定
     func setupPreviewLayer(){
-        //--プレビューレイヤを初期化し、縦横比と向きを設定
+        // プレビューレイヤを初期化し、縦横比と向きを設定
         self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSesion)
         self.cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
         self.cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
         
-        //--viewにpreviewlayerを設定
+        // viewにpreviewlayerを設定
         self.cameraPreviewLayer?.frame = self.imageView.frame
         self.imageView.layer.insertSublayer(self.cameraPreviewLayer!, at: 0)
     }
-
+    
 }
 
-//--
+//
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate{
-    //--
+    //
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        let image:UIImage = captureImage(sampleBuffer: sampleBuffer)
+        let image = captureImage(sampleBuffer: sampleBuffer)
         
         DispatchQueue.main.async {
             self.imageView.image = image
@@ -125,12 +144,21 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate{
 }
 
 extension ViewController{
-    func captureImage(sampleBuffer:CMSampleBuffer) -> UIImage{
-        let imgbuf:CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
-        let ciimg:CIImage = CIImage(cvPixelBuffer: imgbuf)
-        let cicontext = CIContext.init(options: nil)
-        let cgimg = cicontext.createCGImage(ciimg, from: ciimg.extent)!
+    // バッファから画像を取得する
+    func captureImage(sampleBuffer: CMSampleBuffer) -> UIImage?{
+        // イメージバッファを取得
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return nil}
         
-        return UIImage.init(cgImage: cgimg)
+        // CIImageに変換
+        let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+        let ciContext = CIContext.init(options: nil)
+        
+        // CIImageからCGImageを生成
+        let cgImage = ciContext.createCGImage(ciImage, from: ciImage.extent)!
+        
+        let deviceOrientation = UIDevice.current.orientation
+        
+        // UIImageにして返す
+        return UIImage(cgImage: cgImage, scale: 0, orientation: .right)
     }
 }
