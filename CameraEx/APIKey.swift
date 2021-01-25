f200018ac7af6fbc9d03dd440d1c7501dd293261
Enchan <enchant0130@gmail.com>
2021-01-25 09:12:33 +0900

    [Add] APIKey.swift

---
+++ CameraEx/APIKey.swift
@@ -0,0 +1,11 @@
+//
+//  APIKey.swift
+//  CameraEx
+//
+//  Created by EnchantCode on 2021/01/25.
+//  Copyright © 2021 EnchantCode. All rights reserved.
+//
+
+import Foundation
+
+let YAHOO_API_CLIENT_ID="XXXXXXX"
