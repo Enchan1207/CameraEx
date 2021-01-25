0031f8133bb7fb6c5c990096bbeb326d6f985a0e
Enchan <enchant0130@gmail.com>
2021-01-25 09:26:59 +0900

    [Update] search product-info from Yahoo!Shopping API

--- CameraEx/CameraViewController/CameraViewControllerExt.swift
+++ CameraEx/CameraViewController/CameraViewControllerExt.swift
@@ -8,6 +8,7 @@
 
 import AVFoundation
 import UIKit
+import SwiftyJSON
 
 extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate{
     
@@ -24,13 +25,13 @@ extension CameraViewController: AVCaptureMetadataOutputObjectsDelegate{
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         
         // サンプルを一つ取得
-        guard let object = metadataObjects.first else {return}
+        let barcodeObjects = metadataObjects.filter {return [.ean8, .ean13, .code128].contains($0.type)}
+        guard let sampledInstance = barcodeObjects.first else {return}
         
         // boundsとpreviewViewのframeからpreviewView上の位置を計算、設定
         // metadataOutputはorientationを変更できないので、適宜座標軸を変更する必要がある
-        let bounds = object.bounds
+        let bounds = sampledInstance.bounds
         let previewFrame = previewView.frame
-        
         let startPoint = CGPoint(
             x: (1 - bounds.maxY) * previewFrame.width,
             y: bounds.minX * previewFrame.height
@@ -39,15 +40,30 @@ extension CameraViewController: AVCaptureMetadataOutputObjectsDelegate{
             x: (1 - bounds.minY) * previewFrame.width,
             y: bounds.maxX * previewFrame.height
         )
-        
         let detectRect = CGRect(
             x: startPoint.x,
             y: startPoint.y,
             width: endPoint.x - startPoint.x,
             height: endPoint.y - startPoint.y
         )
-        
         self.detectView.frame = detectRect
         
+        // バーコードの値を取得
+        guard let codeObject = sampledInstance as? AVMetadataMachineReadableCodeObject else {return}
+        guard let barcodeValue = codeObject.stringValue else {return}
+        
+        // 読み取ったことがなければ
+        if(!self.detectedCodes.contains(barcodeValue)){
+            self.detectedCodes.append(barcodeValue)
+            
+            // APIに投げる
+            let endPoint = "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch"
+            guard let response = try? Data(contentsOf: URL(string: "\(endPoint)?appid=\(YAHOO_API_CLIENT_ID)&jan_code=\(barcodeValue)")!) else {return}
+            guard let responseJson = try? JSON(data: response) else {return}
+            
+            // labelに表示
+            let productName = responseJson["hits"][0]["name"].string
+            self.positionLabel.text = productName
+        }
     }
 }
